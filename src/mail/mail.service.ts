0c1487958ac8d346df343ec227c88cba0eed9a08
Niurmiguel Gonzalez <niurmiguelg@gmail.com>
2024-07-06 17:11:51 -0400

    JSON Extraction from remote url

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,9 +2,10 @@ import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nes
 import * as fs from 'fs-extra';
 import axios from 'axios';
 import { simpleParser } from 'mailparser';
+import * as path from 'path';
+import * as cheerio from 'cheerio';
 
 import { AbstractMailService } from './mail.abstract';
-import * as path from 'path';
 
 @Injectable()
 export class MailService implements AbstractMailService {
@@ -19,7 +20,7 @@ export class MailService implements AbstractMailService {
             const rawEmail = this.isLocalUrl(urlOrPath) ? await this.readLocalFile(urlOrPath) : await this.readRemoteFile(urlOrPath);
             const { attachments, text, html } = await simpleParser(rawEmail);
 
-            const jsonContent = this.extractJsonFromAttachments(attachments) || this.extractJsonFromEmailBody(text);
+            const jsonContent = this.extractJsonFromAttachments(attachments) || await this.extractJsonFromEmailBody(text) || await this.extractJsonFromRedirectedPage(text);
 
             return jsonContent;
         } catch (error) {
@@ -48,6 +49,33 @@ export class MailService implements AbstractMailService {
         }
 
         return null;
+    }
+
+    private async extractJsonFromRedirectedPage(emailBody: string): Promise<any> {
+        try {
+            const regex = /((?:https?|ftp):\/\/[^\s/$.?#].[^\s]*)/gi;
+            const matches = emailBody.match(regex) || [];
+
+            if (matches && matches[0]) {
+                const response = await axios.get(matches[0]);
+                const $ = cheerio.load(response.data);
+                let jsonLink = $('a[href$=".json"]').attr('href');
+                if (!jsonLink) {
+                    throw new Error('A JSON link was not found in the redirected page.');
+                }
+                if (jsonLink.startsWith('/')) {
+                    jsonLink = `${response.request.protocol}//${response.request.host}${jsonLink}`
+                }
+                const jsonResponse = await axios.get(jsonLink);
+                return jsonResponse.data;
+            }
+
+            return null;
+
+        } catch (error) {
+            console.error('Error extracting JSON from redirected page:', error.message);
+            return null;
+        }
     }
 
     async readLocalFile(urlOrPath: string): Promise<string> {

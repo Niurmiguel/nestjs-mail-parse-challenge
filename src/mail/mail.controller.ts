6b8152b6e3cb6e28896c440767bb034c8a42b0b9
Niurmiguel Gonzalez <niurmiguelg@gmail.com>
2024-07-06 15:38:02 -0400

    JSON Extraction from Attachments

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, HttpException, HttpStatus } from '@nestjs/common';
 import { AbstractMailService } from './mail.abstract';
 
 @Controller('mail-parsing')
@@ -7,6 +7,12 @@ export class MailController {
 
     @Get(':urlOrPath')
     async getEmail(@Param('urlOrPath') urlOrPath: string): Promise<any> {
-        return await this.mailService.parseEmail(urlOrPath);
+        try {
+            const parsedEmail = await this.mailService.parseEmail(urlOrPath);
+            return parsedEmail;
+        } catch (error) {
+            // Handle specific errors or generalize as needed
+            throw new HttpException(`Failed to parse email: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import * as fs from 'fs-extra';
 import axios from 'axios';
+import { simpleParser } from 'mailparser';
 
 import { AbstractMailService } from './mail.abstract';
 
@@ -8,17 +9,31 @@ import { AbstractMailService } from './mail.abstract';
 export class MailService implements AbstractMailService {
     async parseEmail(urlOrPath: string): Promise<any> {
         try {
-            const rawEmail = this.isLocalUrl(urlOrPath) ? await this.readLocalFile(urlOrPath) : await this.readRemoteFile(urlOrPath)
+            const rawEmail = this.isLocalUrl(urlOrPath) ? await this.readLocalFile(urlOrPath) : await this.readRemoteFile(urlOrPath);
+            const { attachments, text, html } = await simpleParser(rawEmail);
 
-            return rawEmail;
+            const jsonContent = this.extractJsonFromAttachments(attachments);
+
+            return jsonContent;
         } catch (error) {
-            throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new HttpException(`Error parsing email: ${error.message}`, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private extractJsonFromAttachments(attachments: any[]): any {
+        if (attachments && attachments.length > 0) {
+            for (const attachment of attachments) {
+                if (attachment.contentType === 'application/json') {
+                    return JSON.parse(attachment.content.toString());
+                }
+            }
+        }
+        return null;
     }
 
     async readLocalFile(urlOrPath: string): Promise<string> {
         try {
-            return fs.readFileSync(urlOrPath, 'utf8');
+            return await fs.readFile(urlOrPath, 'utf8');
         } catch (error) {
             throw new Error(`Error reading local file: ${error.message}`);
         }
@@ -27,8 +42,6 @@ export class MailService implements AbstractMailService {
     async readRemoteFile(urlOrPath: string): Promise<string> {
         try {
             const response = await axios.get(urlOrPath);
-            console.log('Contenido del archivo remoto:');
-            console.log(response.data);
             return response.data;
         } catch (error) {
             throw new Error(`Error reading remote file: ${error.message}`);
@@ -36,10 +49,6 @@ export class MailService implements AbstractMailService {
     }
 
     isLocalUrl(urlOrPath: string): boolean {
-        if (urlOrPath.startsWith('file://') || urlOrPath.startsWith('http://localhost') || urlOrPath.startsWith('http://127.0.0.1') || !urlOrPath.includes('://')) {
-            return true;
-        }
-
-        return false;
+        return !urlOrPath.includes('://') || urlOrPath.startsWith('file://') || urlOrPath.startsWith('http://localhost') || urlOrPath.startsWith('http://127.0.0.1');
     }
 }
